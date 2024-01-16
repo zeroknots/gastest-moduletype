428f88bf54b67f5e582b59aab2dc17fbc410251e
zeroknots <zeroknots@protonmail.com>
2024-01-16 17:24:06 +0700

    foo

---
+++ src/ModuleType.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.21;
+
+type ModuleTypesEnc is uint256;
+
+type ModuleType is uint256;
+
+contract Module {
+    ModuleTypesEnc public constant MODULE_TYPES = ModuleTypesEnc.wrap(30);
+
+    function isModuleType(ModuleType _isModuleType) public pure returns (bool) {
+        return ModuleTypeLib.isType(MODULE_TYPES, _isModuleType);
+    }
+
+    function getModuleTypesArray() public pure returns (uint256[] memory moduleTypes) {
+        moduleTypes = new uint256[](4);
+        moduleTypes[0] = 1;
+        moduleTypes[1] = 2;
+        moduleTypes[2] = 3;
+        moduleTypes[3] = 4;
+    }
+
+    function getModuleTypes() public pure returns (ModuleTypesEnc) {
+        return MODULE_TYPES;
+    }
+}
+
+library ModuleTypeLib {
+    function isType(ModuleTypesEnc self, ModuleType moduleType) internal pure returns (bool) {
+        return (ModuleTypesEnc.unwrap(self) & 2 ** ModuleType.unwrap(moduleType)) != 0;
+    }
+
+    function bitEncode(ModuleType[] memory moduleTypes) internal pure returns (ModuleTypesEnc) {
+        uint16 result;
+        for (uint256 i; i < moduleTypes.length; i++) {
+            result = result + uint16(2 ** ModuleType.unwrap(moduleTypes[i]));
+        }
+        return ModuleTypesEnc.wrap(result);
+    }
+
+    function bitEncodeCalldata(ModuleType[] calldata moduleTypes) internal pure returns (ModuleTypesEnc) {
+        uint16 result;
+        for (uint256 i; i < moduleTypes.length; i++) {
+            result = result + uint16(2 ** ModuleType.unwrap(moduleTypes[i]));
+        }
+        return ModuleTypesEnc.wrap(result);
+    }
+}
---
+++ test/ModuleType.t.sol
@@ -0,0 +1,50 @@
+import "src/ModuleType.sol";
+
+import "forge-std/Test.sol";
+
+contract ModuleTypeTest is Test {
+    using ModuleTypeLib for *;
+
+    Module module;
+
+    ModuleType[] typesArray;
+
+    function setUp() public {
+        module = new Module();
+
+        typesArray = new ModuleType[](4);
+        typesArray[0] = ModuleType.wrap(1);
+        typesArray[1] = ModuleType.wrap(2);
+        typesArray[2] = ModuleType.wrap(3);
+        typesArray[3] = ModuleType.wrap(4);
+    }
+
+    function test_encoding() public {
+        ModuleTypesEnc encoded = typesArray.bitEncode();
+        console2.log(ModuleTypesEnc.unwrap(encoded));
+    }
+
+    function test_array() public {
+        uint256 _gas = gasleft();
+        // check if module is of type 3;
+        uint256[] memory types = module.getModuleTypesArray();
+        uint256 length = types.length;
+        bool isType = false;
+        for (uint256 i; i < length; i++) {
+            if (types[i] == 3) {
+                isType = true;
+                break;
+            }
+        }
+        _gas = _gas - gasleft();
+        console2.log("array", isType, _gas);
+    }
+
+    function test_value() public {
+        uint256 _gas = gasleft();
+        ModuleTypesEnc types = module.getModuleTypes();
+        bool isType = types.isType(ModuleType.wrap(3));
+        _gas = _gas - gasleft();
+        console2.log("value", isType, _gas);
+    }
+}
